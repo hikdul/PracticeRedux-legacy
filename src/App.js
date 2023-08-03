dd48ba65c9bfa002f7fc398f0432d8fdd9139893
hikdul <hikdul.lio@gmail.com>
2023-08-03 16:10:44 -0500

    completando segun lo que indica Nicolas

--- src/App.js
+++ src/App.js
@@ -1,46 +1,104 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-export const reducer = (state = 0, action) =>{
+const initialState = {
+  entities: [],
+  filter: 'all'// complete, incomplete
+}
+// ** un todo debe de completar los siguientes datos {Id: number, title: string, completed: bool}
 
+// ! evitar la mutabilidad en reducer, siempo devolver una nueva copia del elemento o del estado... esto motivado a que reducer no re-renderizan si no es un estado nuevo
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'up':{
-      return state + 1
+    case 'todo/add':{
+      return{
+        ...state,
+        entities: state.entities.concat({...action.payload })
+      }
     }
-    case 'down':{
-      return state - 1
+    case 'todo/complete': {
+      const newTodos = state.entities.map(todo => {
+        if(todo.id === action.payload.id)
+          return {...todo, completed: !todo.completed }
+        return todo
+      })
+     return {
+      ...state,
+      entities: newTodos
+     }
     }
-    case 'set':{
-      return action.payload
+    case 'filte/set': {
+      return {
+        ...state,
+        filter: action.payload
+      }
     }
-    default: {
+    default:{
       return state
     }
   }
-  return state
 }
 
-function App() {
+const selectTodos = state =>{
+  const{entities, filter} = state
+  
+  if(filter == 'complete')
+    return entities.filter(todo => todo.completed)
+  if(filter == 'incomplete')
+    return entities.filter(todo => !todo.completed)
+  return entities
+}
+
+const TodoItem = ({todo}) => 
+{
+  const dispatch = useDispatch()
+  return(
+    <li
+      style={{textDecoration: todo.completed ? 'line-through' : 'none'}}
+      onClick={()=> dispatch({type:'todo/complete', payload:todo})}
+    > <b>{todo.id}:</b> {todo.title} </li>
+  )
+}
+
+const App = () =>
+{
 
   const [value, setValue] = useState('')
   const dispatch = useDispatch()
-  const state = useSelector(state => state)
-  const set = () => {
-    dispatch({type:'set', payload: value})
+  //  ** --> // la idea en la siguiente linea es que obtiene el estado completo.
+  //  ** --> // const state = useSelector(x => x)
+  // ** --> // en la siguiente linea hacemos uso del hock que es la idea de este
+  //const state = useSelector(selectTodos)
+  const todos = useSelector(selectTodos)
+  
+  const submit = e => {
+
+    e.preventDefault()
+
+    if(!value.trim())
+      return
+
+    const Id = Math.random().toString(36).substring( 3,13)
+    //const todo = {title: value, completed: false, id: Id}
+    //console.log({todo})
+    dispatch({ type: 'todo/add', payload: {title: value, completed: false, id: Id}})
     setValue('')
-  } 
+  }
 
   return (
     <div>
-      <p>Counter: {state}</p>
-      <button onClick={() => dispatch({type:'up'})}>+</button>
-      <button onClick={() => dispatch({type:'down'})}>-</button>
-      <label>
-        <input value={value} onChange={e => setValue(Number(e.target.value))}/>
-        <button onClick={set} >Set</button>
-      </label>
+      <form onSubmit={submit}>
+        <input value={value} onChange={e => setValue(e.target.value)}/>
+        <button type='submit' >+</button>
+      </form>
+      <button onClick={() => dispatch({type:'filter/set', payload:'all'})} > all </button>
+      <button onClick={() => dispatch({type:'filter/set', payload:'complete'})}> complete </button>
+      <button onClick={() => dispatch({type:'filter/set', payload:'incomplete'})}> incomplete </button>
+      <ul>
+        {todos.map( todo => <TodoItem key={todo.id} todo={todo}/> )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
